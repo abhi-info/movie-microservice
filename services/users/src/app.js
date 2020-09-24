31237bbb5e3a78edd15f43406942073710c5a2db
Abhishek Rawat <rawat_abhishek@infoobjects.com>
2020-09-24 18:36:08 +0530

    Updated docker file for user microservice

--- services/users/src/app.js
+++ services/users/src/app.js
@@ -7,7 +7,7 @@ const routes = require('./routes/users');
 
 const app = express();
 
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || '3005';
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
